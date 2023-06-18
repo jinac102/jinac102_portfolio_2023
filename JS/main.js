3ff7023a445b19de218a9f37c39cf5fb82e424e8
jinac102 <monet158@naver.com>
2023-06-19 00:29:38 +0900

    teamPJ 폴더 수정 및 디테일 페이지 추가 작업

--- JS/main.js
+++ JS/main.js
@@ -67,3 +67,23 @@ lang.addEventListener("click", function () {
     this.classList.remove("tog_open");
   });
 });
+
+/* sec02 */
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // autoplay: {
+  //   delay: 000,
+  // },
+  mousewheel: true,
+  keyboard: true,
+  rewind: true,
+});
